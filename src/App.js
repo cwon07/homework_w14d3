c87436c287455d2746f0c786ceed4982f1497a36
Christine Wong <jumiryoo@gmail.com>
2023-08-01 00:37:44 -0700

    web app in working order minus styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import Counter from './components/Counter';
 import Trivia from './components/Trivia';
-import TriviaClue from './components/TriviaClue';
-import TriviaReveal from './components/TriviaReveal';
-import {useEffect} from 'react';
 
-  const API_URL = "http://jservice.io/api/random"
+
 
   const App = () => {
-
-    const randomTrivia = async (question) => {
-        const response = await fetch(`${API_URL}`);
-        const data = await response.json();
-
-        console.log(data);
-    }
-
-    useEffect(() => {
-        randomTrivia('Wine');
-    }, []);
 
   return(
 
     <div className="App">
       <Counter />
       <Trivia />
-      <TriviaClue />
-      <TriviaReveal />
     </div>
   );
 }
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Trivia = () => {
+  const [trivia, setTrivia] = useState({});
+  const [revealQuestion, setRevealQuestion] = useState(false);
+
+  const randomTrivia = async () => {
+    const response = await fetch("http://jservice.io/api/random");
+    const data = await response.json();
+    setTrivia(data[0]);
+    setRevealQuestion(true);
+  };
+
+  useEffect(() => {
+    randomTrivia();
+  }, []);
+
+  const revealQuestionHandler = () => {
+    setRevealQuestion(!revealQuestion);
+  };
+
     return (
         <>
-        <div className = "getQuestion">
+          <div className="getQuestion">
             <h2>Let's Play!</h2>
-            <button>Get Question</button>
-        </div>        
+            <button onClick={randomTrivia}>Get Question</button>
+            {revealQuestion && (
+              <>
+                <h3>Category: {trivia.category.title}</h3>
+                <h4>Points: {trivia.value}</h4>
+                <h4>Answer: {trivia.answer}</h4>
+                <button onClick={revealQuestionHandler}>Click to Reveal Question</button>                
+              </>
+            )}
+            {revealQuestion && <p>{trivia.question}</p>}
+          </div>
         </>
-    )
-}
+      );
+};
 
 export default Trivia;
